5511a05f9ae756a5a7573850e41da94c001130fd
Alan-code07 <alant5672@gmail.com>
2025-11-14 19:05:32 -0300

    Add files via upload

---
+++ java/script.js
@@ -0,0 +1,6 @@
+let nomeAtual = "Alan Lindo"; // valor inicial
+
+function mudarNome() {
+    let nome = "Alan Lindo";
+    document.getElementById("mensagemSaudacao").textContent = "Olá, " + nome + " tudo bom?";
+}
